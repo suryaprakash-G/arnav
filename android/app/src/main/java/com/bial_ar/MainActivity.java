b2dbe97c53585042030ffa27b03eeb52f35d9631
suryaprakash-G <suryagopinathan@gmail.com>
2022-01-04 17:18:49 +0530

    fresh app for deeplinking

---
+++ android/app/src/main/java/com/bial_ar/MainActivity.java
@@ -0,0 +1,15 @@
+package com.bial_ar;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "Bial_AR";
+    }
+}
